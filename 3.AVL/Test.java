e809f611384632c326b84cf496975854e52e55c7
Gabriel <gmamanican@unsa.edu.pe>
2022-06-28 19:59:11 -0500

    adding test with a big tree

--- 3.AVL/Test.java
+++ 3.AVL/Test.java
@@ -47,6 +47,27 @@ public class Test {
         AVL<Integer> avl2 = new AVL<Integer>();
 
         System.out.println(avl2.toString());
+        System.out.println("=====================================");
+        // test with big tree
+        AVL<Integer> bigAvl = new AVL<Integer>();
+        int[] elements = { 100, 200, 300, 400, 500, 50, 25, 350, 375, 360, 355, 150, 175, 120, 190 };
+        try {
+            // adding elements to the tree
+            System.out.println("Adding elements to the tree");
+            for (int element : elements) {
+                bigAvl.insert(element);
+                System.out.println(bigAvl.toString());
+            }
+            System.out.println("=====================================");
+            // removing elements from the tree
+            System.out.println("Removing elements from the tree");
+            for (int element : elements) {
+                bigAvl.remove(element);
+                System.out.println(bigAvl.toString());
+            }
 
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
